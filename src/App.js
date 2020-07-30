45b0223ff505aee6e7b54eb45d4a1ab6f37270d7
keatonBraith <keeters33@live.com>
2020-07-29 18:44:02 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dropdownVisible: false,
+      clockwise: true,
+
+    };
+  }
+
+    toggleDropDown = () => {
+      this.setState({
+        dropdownVisible: !this.state.dropdownVisible,
+      });
+    };
+
+    toggleSpin = () => {
+      this.setState({
+        clockwise: !this.state.clockwise
+      })
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="img-box">
+            <header className="header">
+              <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>
+              <nav className="desktop-menu">
+                <span>SERVICES</span>
+                <span>PORTFOLIO</span>
+                <span>ABOUT</span>
+                <span>TEAM</span>
+                <span>CONTACT</span>
+              </nav>
+              <div className="dropdown" onClick={this.toggleDropDown}>
+              MENU &#9776; 
+          </div>
+          {this.state.dropdownVisible ? (
+            <nav className="mobile-menu navbar-collapse">
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+          ) : null}
+            </header>
+            <main className="body">
+              <p className="welcome">Welcome To Our Studio!</p>
+              <p className="meet">IT'S NICE TO MEET YOU</p>
+              <button className="button">TELL ME MORE</button>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
